0e98b2d34dbc260ca37976e60822ef2262ad5575
DANIEL J <86760872+danielAnandhaGeethan@users.noreply.github.com>
2024-04-20 22:13:37 +0530

    Update Buttons.jsx

--- client/src/components/Buttons.jsx
+++ client/src/components/Buttons.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Buttons = ({ walletAddress, logout, disconnect }) => {
   return (
-    <div>
+    <>
       {window.location.href !== "http://localhost:3000/" ? (
         <div className="cursor-pointer" onClick={logout}>
           <h1 className="font-sans text-white text-[15px] bg-black px-2 py-1 mr-5 mt-4 rounded-xl text-center">
@@ -22,7 +22,7 @@ const Buttons = ({ walletAddress, logout, disconnect }) => {
       ) : (
         ""
       )}
-    </div>
+    </>
   );
 };
 
